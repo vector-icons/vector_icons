0cc0433de3f8756b0cc84a012cecd7f21ae11297
Dev TtangKong <ttankkeo112@gmail.com>
2024-08-10 12:36:58 +0900

    Update about viewer

--- view/index.js
+++ view/index.js
@@ -1,4 +1,5 @@
 const selector = document.getElementById("icon-selector");
+const viewer = document.getElementById("viewer");
 
 fetch("../assets/icons.json").then(async response => {
     const iconJson = await response.json();
@@ -8,10 +9,33 @@ fetch("../assets/icons.json").then(async response => {
         const name = icon[0];
         const path = icon[1];
 
-        const option = document.createElement("option");
-        option.textContent = name;
-        option.setAttribute("path", path);
-        
-        selector.appendChild(option);
+        {
+            const option = document.createElement("option");
+            option.textContent = name;
+            option.setAttribute("path", path);
+            
+            selector.appendChild(option);
+        }
+        {
+            const r1 = await fetch("../assets/" + path["normal"]);
+            const r2 = await fetch("../assets/" + path["filled"]);
+
+            const div = document.createElement("div");
+            div.style.display = "flex";
+            div.style.flexDirection = "column";
+            div.style.width = "32px";
+            div.style.width = "32px";
+            div.style.gap = "5px";
+
+            const svg1 = await r1.text();
+            const svg2 = await r2.text();
+
+            div.innerHTML = `
+                ${svg1}
+                ${svg2}
+            `;
+
+            viewer.append(div);
+        }
     }
 });
